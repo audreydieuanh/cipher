3096d48bdf862c75ec97cd0dceca923bbb58dc16
Dieu-Anh (Audrey) Trinh <127930337+audreydieuanh@users.noreply.github.com>
2023-11-09 22:04:04 -0600

    Change for redo 1

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -12,34 +12,34 @@ The only difference is to add/subtract, I group it into 1 method to remove repea
 
 /**
  * This class encodes and decodes string using a number
- * 
+ *
  * @author Audrey Trinh
  */
 
 public class CaesarCipher {
   public static void main(String[] args) {
-      // if there are incorrect numbers of command-line arguments
-      if (args.length != 2) {
-          // state that the input was invalid
-          System.err.println("Incorrect number of parameters. Please only enter two, separated by white space.");
-          // exit with a code of 2
-          System.exit(2);
-      }
+    // if there are incorrect numbers of command-line arguments
+    if (args.length != 2) {
+      // state that the input was invalid
+      System.err.println("Incorrect number of parameters. Please only enter two, separated by white space.");
+      // exit with a code of 2
+      System.exit(2);
+    }
 
-      if (!args[0].equals("encode") && !args[0].equals("decode")) {
-        // state that the input was invalid
-        System.err.println("The input was invalid. Valid options are 'encode' or 'decode'");
-        // exit with a code of 1
-        System.exit(1);
-      }
+    if (!args[0].equals("encode") && !args[0].equals("decode")) {
+      // state that the input was invalid
+      System.err.println("The input was invalid. Valid options are 'encode' or 'decode'");
+      // exit with a code of 1
+      System.exit(1);
+    }
 
-      // turn the string into a char array
-      char[] message = args[1].toCharArray();
+    // turn the string into a char array
+    char[] message = args[1].toCharArray();
 
-      // change for redo 1, change variable name
-      for (int key = 0; key < 26; key++) {
-        encodeOrdecode(args[0], message, key);
-      }
+    // change for redo 1, change variable name
+    for (int key = 0; key < 26; key++) {
+      encodeOrdecode(args[0], message, key);
+    }
 
   } // main function
 
@@ -48,7 +48,7 @@ public class CaesarCipher {
    * post-condition: encode/decode he str by adding/subtracting n to each character in str
    */
 
-   // change for redo 1, make 2 methods encode and decode into 1 method to remove repeated code
+  // change for redo 1, make 2 methods encode and decode into 1 method to remove repeated code
   public static void encodeOrdecode(String request, char[] message, int key) {
     char[] result = new char[message.length];
     for (int i = 0; i < result.length; i++) {
@@ -61,8 +61,7 @@ public class CaesarCipher {
 
     String resultStr = new String(result);
     System.out.println("n = " + key + ": " + resultStr);
-   }
-
+  }
 
 
   /*
@@ -71,15 +70,15 @@ public class CaesarCipher {
    * 26 characters and wrap around if not
    */
   public static int modulo(int ch) {
-      ch -= 'a';
-      // if ch is in the range
-      if (ch >= 0) {
-          // return the original ch
-          return ch % 26 + 'a';
-          // if not
-      } else {
-          // wrap around the lowercase alphabet
-          return ch % 26 + 26 + 'a';
-      }
+    ch -= 'a';
+    // if ch is in the range
+    if (ch >= 0) {
+      // return the original ch
+      return ch % 26 + 'a';
+      // if not
+    } else {
+      // wrap around the lowercase alphabet
+      return ch % 26 + 26 + 'a';
+    }
   }
 }
--- VigenereCipher.java
+++ VigenereCipher.java
@@ -5,53 +5,53 @@
    1.3 str -> message (encodeOrdecode method)
 
 2. Change method implementation:
-Since encode and decode is mostly similar. 
+Since encode and decode is mostly similar.
 The only difference is to add/subtract, I group it into 1 method to remove repeated code.
  */
 
 
 /**
  * This class encodes and decodes string using a keyword
- * 
+ *
  * @author Audrey Trinh
  */
 
 public class VigenereCipher {
   public static void main(String[] args) {
-      // if there are incorrect numbers of command-line arguments
-      if (args.length != 3) {
-          // state that the input was invalid
-          System.err.println("Incorrect number of parameters. Please only enter 3, separated by white space.");
-          // exit with a code of 2
-          System.exit(2);
-      }
+    // if there are incorrect numbers of command-line arguments
+    if (args.length != 3) {
+      // state that the input was invalid
+      System.err.println("Incorrect number of parameters. Please only enter 3, separated by white space.");
+      // exit with a code of 2
+      System.exit(2);
+    }
 
-      if (!args[0].equals("encode") && !args[0].equals("decode")) {
-        // state that the input was invalid
-        System.err.println("The input was invalid. Valid options are 'encode' or 'decode'");
-        // exit with a code of 1
-        System.exit(1);
-      }
+    if (!args[0].equals("encode") && !args[0].equals("decode")) {
+      // state that the input was invalid
+      System.err.println("The input was invalid. Valid options are 'encode' or 'decode'");
+      // exit with a code of 1
+      System.exit(1);
+    }
 
-      // if keyword is an empty string
-      if (args[2].equals("")) {
-              // print the original string
-              System.out.println(args[1]);
-              return;
-          }
-      // turn the string into a char array
-      char[] message = args[1].toCharArray();
+    // if keyword is an empty string
+    if (args[2].equals("")) {
+      // print the original string
+      System.out.println(args[1]);
+      return;
+    }
+    // turn the string into a char array
+    char[] message = args[1].toCharArray();
 
-      // turn the keyword into a char array
-      char[] keyWord = args[2].toCharArray();
+    // turn the keyword into a char array
+    char[] keyWord = args[2].toCharArray();
 
-      char[] keyRepl = new char[message.length];
-      // replicate the keyword so that it has the same size as the string
-      for (int i = 0; i < keyRepl.length; i++) {
-          keyRepl[i] = keyWord[i % keyWord.length];
-      }
+    char[] keyRepl = new char[message.length];
+    // replicate the keyword so that it has the same size as the string
+    for (int i = 0; i < keyRepl.length; i++) {
+      keyRepl[i] = keyWord[i % keyWord.length];
+    }
 
-      encodeOrDecode(args[0], message, keyRepl);
+    encodeOrDecode(args[0], message, keyRepl);
   }
 
   /*
@@ -60,7 +60,7 @@ public class VigenereCipher {
    * each character in str
    */
 
-   // change for redo 1, make 2 methods encode and decode into 1 method to remove repeated code
+  // change for redo 1, make 2 methods encode and decode into 1 method to remove repeated code
   public static void encodeOrDecode(String request, char[] message, char[] keyRepl) {
     // make a new char array with the same size as str
     char[] result = new char[message.length];
@@ -76,7 +76,7 @@ public class VigenereCipher {
     String resultStr = new String(result);
     // print the encoded string
     System.out.println(resultStr);
-}
+  }
 
   /*
    * pre-condition: ch is a lowercase character
@@ -84,15 +84,16 @@ public class VigenereCipher {
    * 26 characters and wrap around if not
    */
   public static int modulo(int ch) {
-      ch -= 'a';
-      // if ch is in the range
-      if (ch >= 0) {
-          // return the original ch
-          return ch % 26 + 'a';
-          // if not
-      } else {
-          // wrap around the lowercase alphabet
-          return ch % 26 + 26 + 'a';
-      }
+    ch -= 'a';
+    // if ch is in the range
+    if (ch >= 0) {
+      // return the original ch
+      return ch % 26 + 'a';
+      // if not
+    } else {
+      // wrap around the lowercase alphabet
+      return ch % 26 + 26 + 'a';
+    }
   }
 }
+
